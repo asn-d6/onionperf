aac7334748e3e4791c8e0f901e2f1683e5b6fc4b
Ana C. Custura <ana@netstat.org.uk>
2019-05-06 15:04:30 +0100

    Adds a reprocessing module

---
+++ onionperf/reprocessing.py
@@ -0,0 +1,70 @@
+from onionperf.analysis import Analysis
+from onionperf import util
+from functools import partial
+from multiprocessing import Pool, cpu_count
+import datetime
+import fnmatch
+import logging
+import os
+import re
+
+def collect_logs(dirpath, pattern):
+    logs = []
+    for root, dirnames, filenames in os.walk(dirpath):
+        for filename in fnmatch.filter(filenames, pattern):
+            logs.append(os.path.join(root, filename))
+    return logs
+    
+
+def match(tgen_logs, tor_logs, date_filter):
+    log_pairs = []
+    for tgen_log in tgen_logs:
+        m = re.search(r'(\d+-\d+-\d+)', tgen_log)
+        if m:
+            date = m.group(0)
+            fdate = datetime.datetime.strptime(date, "%Y-%m-%d")
+            found = False
+            if date_filter is None or util.do_dates_match(date_filter,fdate):
+                for tor_log in tor_logs:
+                    if date in tor_log:
+                        log_pairs.append((tgen_log, tor_log, fdate))
+                        found = True
+                        break                       
+                if not found:
+                    logging.warning('Skipping file {0}, could not find a match for it'.format(tgen_log))
+                           
+        else:
+            logging.warning('Filename {0} does not contain a date'.format(tgen_log))
+    if not log_pairs:
+       logging.warning('Could not find any log matches. No analyses will be performed')
+    return log_pairs
+ 
+
+def analyze_func(prefix, pair):
+    analysis = Analysis()
+    logging.info('Analysing pair for date {0}'.format(pair[2]))
+    analysis.add_tgen_file(pair[0])
+    analysis.add_torctl_file(pair[1])
+    analysis.analyze(do_simple=False, date_filter=pair[2])
+    analysis.save(output_prefix=prefix)
+    analysis.export_torperf_version_1_1(output_prefix=prefix, do_compress=False)
+    return 1
+ 
+ 
+def multiprocess_logs(log_pairs, prefix):
+    pool = Pool(cpu_count())
+    analyses = None
+    try:
+        func = partial(analyze_func, prefix)
+        mr = pool.map_async(func, log_pairs)
+        pool.close()
+        while not mr.ready(): mr.wait(1)
+    except KeyboardInterrupt:
+        logging.info("interrupted, terminating process pool")
+        pool.terminate()
+        pool.join()
+        sys.exit()
+    except Exception as e:
+        logging.error(e)
+
+   
---
+++ onionperf/tests/test_reprocessing.py
@@ -0,0 +1,94 @@
+import os
+import pkg_resources
+import datetime
+import tempfile
+import shutil
+from nose.tools import *
+from onionperf import analysis
+from onionperf import reprocessing
+
+
+def absolute_data_path(relative_path=""):
+    """
+    Returns an absolute path for test data given a relative path.
+    """
+    return pkg_resources.resource_filename("onionperf",
+                                           "tests/data/" + relative_path)
+
+
+DATA_DIR = absolute_data_path()
+
+def test_log_collection_tgen():
+    log_list = reprocessing.collect_logs(DATA_DIR, '*tgen.log')
+    well_known_list = [ DATA_DIR + 'logs/onionperf.tgen.log', DATA_DIR + 'logs/onionperf_2019-01-10_23:59:59.tgen.log' ]
+    assert_equals(log_list, well_known_list )
+
+def test_log_collection_torctl():
+    log_list = reprocessing.collect_logs(DATA_DIR, '*torctl.log')
+    well_known_list = [ DATA_DIR + 'logs/onionperf.torctl.log', DATA_DIR + 'logs/onionperf_2019-01-10_23:59:59.torctl.log' ]
+    assert_equals(log_list, well_known_list )
+
+def test_log_match():
+    tgen_logs = reprocessing.collect_logs(DATA_DIR, '*tgen.log')
+    torctl_logs = reprocessing.collect_logs(DATA_DIR, '*torctl.log')
+    log_pairs =  reprocessing.match(tgen_logs, torctl_logs, None)
+    well_known_list = [(DATA_DIR + 'logs/onionperf_2019-01-10_23:59:59.tgen.log', DATA_DIR + 'logs/onionperf_2019-01-10_23:59:59.torctl.log', datetime.datetime(2019, 1, 10, 0, 0))]
+    assert_equals(log_pairs, well_known_list)
+
+def test_log_match_no_log_date():
+    tgen_logs = reprocessing.collect_logs(DATA_DIR, '*perf.tgen.log')
+    torctl_logs = reprocessing.collect_logs(DATA_DIR, '*perf.torctl.log')
+    log_pairs =  reprocessing.match(tgen_logs, torctl_logs, None)
+    well_known_list = []
+    assert_equals(log_pairs, well_known_list)
+
+def test_log_match_with_filter_date():
+    tgen_logs = reprocessing.collect_logs(DATA_DIR, '*tgen.log')
+    torctl_logs = reprocessing.collect_logs(DATA_DIR, '*torctl.log')
+    test_date = datetime.date(2019, 01, 10)
+    log_pairs =  reprocessing.match(tgen_logs, torctl_logs, test_date)
+    well_known_list = [(DATA_DIR + 'logs/onionperf_2019-01-10_23:59:59.tgen.log', DATA_DIR + 'logs/onionperf_2019-01-10_23:59:59.torctl.log', datetime.datetime(2019, 1, 10, 0, 0))]
+    assert_equals(log_pairs, well_known_list)
+
+def test_log_match_with_wrong_filter_date():
+    tgen_logs = reprocessing.collect_logs(DATA_DIR, '*tgen.log')
+    torctl_logs = reprocessing.collect_logs(DATA_DIR, '*torctl.log')
+    test_date = datetime.date(2017, 01, 01)
+    log_pairs =  reprocessing.match(tgen_logs, torctl_logs, test_date)
+    well_known_list = []
+    assert_equals(log_pairs, well_known_list)
+
+def test_analyze_func_json():
+    pair = (DATA_DIR + 'logs/onionperf_2019-01-10_23:59:59.tgen.log', DATA_DIR + 'logs/onionperf_2019-01-10_23:59:59.torctl.log', datetime.datetime(2019, 1, 10, 0, 0))
+    work_dir = tempfile.mkdtemp()
+    reprocessing.analyze_func(work_dir, pair) 
+    json_file = os.path.join(work_dir, "2019-01-10.onionperf.analysis.json.xz")
+    assert(os.path.exists(json_file))
+    for i in ['51200',  '5242880', '1048576']: 
+       torperf_file = os.path.join(work_dir, "op-ab-{0}-2019-01-10.tpf".format(i))
+       assert(os.path.exists(torperf_file))
+    shutil.rmtree(work_dir)
+
+def test_multiprocess_logs():
+    pairs = [(DATA_DIR + 'logs/onionperf_2019-01-10_23:59:59.tgen.log', DATA_DIR + 'logs/onionperf_2019-01-10_23:59:59.torctl.log', datetime.datetime(2019, 1, 10, 0, 0))]
+    work_dir = tempfile.mkdtemp()
+    reprocessing.multiprocess_logs(pairs, work_dir) 
+    json_file = os.path.join(work_dir, "2019-01-10.onionperf.analysis.json.xz")
+    assert(os.path.exists(json_file))
+    for i in ['51200',  '5242880', '1048576']: 
+       torperf_file = os.path.join(work_dir, "op-ab-{0}-2019-01-10.tpf".format(i))
+       assert(os.path.exists(torperf_file))
+    shutil.rmtree(work_dir)
+
+def test_end_to_end():
+    tgen_logs = reprocessing.collect_logs(DATA_DIR, '*tgen.log')
+    torctl_logs = reprocessing.collect_logs(DATA_DIR, '*torctl.log')
+    log_pairs =  reprocessing.match(tgen_logs, torctl_logs, None)
+    work_dir = tempfile.mkdtemp()
+    reprocessing.multiprocess_logs(log_pairs, work_dir) 
+    json_file = os.path.join(work_dir, "2019-01-10.onionperf.analysis.json.xz")
+    assert(os.path.exists(json_file))
+    for i in ['51200',  '5242880', '1048576']: 
+       torperf_file = os.path.join(work_dir, "op-ab-{0}-2019-01-10.tpf".format(i))
+       assert(os.path.exists(torperf_file))
+    shutil.rmtree(work_dir)
