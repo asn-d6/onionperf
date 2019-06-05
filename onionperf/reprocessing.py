096c2bb23d79033be79503754e180145879d3449
Ana C. Custura <ana@netstat.org.uk>
2019-06-05 15:07:10 +0100

    Allows source to be given as a parameter on the commandline

--- onionperf/reprocessing.py
+++ onionperf/reprocessing.py
@@ -7,6 +7,8 @@ import fnmatch
 import logging
 import os
 import re
+import sys
+
 
 def collect_logs(dirpath, pattern):
     logs = []
@@ -14,7 +16,7 @@ def collect_logs(dirpath, pattern):
         for filename in fnmatch.filter(filenames, pattern):
             logs.append(os.path.join(root, filename))
     return logs
-    
+
 
 def match(tgen_logs, tor_logs, date_filter):
     log_pairs = []
@@ -24,41 +26,47 @@ def match(tgen_logs, tor_logs, date_filter):
             date = m.group(0)
             fdate = datetime.datetime.strptime(date, "%Y-%m-%d")
             found = False
-            if date_filter is None or util.do_dates_match(date_filter,fdate):
+            if date_filter is None or util.do_dates_match(date_filter, fdate):
                 for tor_log in tor_logs:
                     if date in tor_log:
                         log_pairs.append((tgen_log, tor_log, fdate))
                         found = True
-                        break                       
+                        break
                 if not found:
-                    logging.warning('Skipping file {0}, could not find a match for it'.format(tgen_log))
-                           
-        else:
-            logging.warning('Filename {0} does not contain a date'.format(tgen_log))
-    if not log_pairs:
-       logging.warning('Could not find any log matches. No analyses will be performed')
-    return log_pairs
- 
+                    logging.warning(
+                        'Skipping file {0}, could not find a match for it'.
+                        format(tgen_log))
 
-def analyze_func(prefix, pair):
-    analysis = Analysis()
+        else:
+            logging.warning(
+                'Filename {0} does not contain a date'.format(tgen_log))
+    if not log_pairs:
+        logging.warning(
+            'Could not find any log matches. No analyses will be performed')
+    return log_pairs
+
+
+def analyze_func(prefix, nick, pair):
+    analysis = Analysis(nickname=nick)
     logging.info('Analysing pair for date {0}'.format(pair[2]))
     analysis.add_tgen_file(pair[0])
     analysis.add_torctl_file(pair[1])
     analysis.analyze(do_simple=False, date_filter=pair[2])
     analysis.save(output_prefix=prefix)
-    analysis.export_torperf_version_1_1(output_prefix=prefix, do_compress=False)
+    analysis.export_torperf_version_1_1(
+        output_prefix=prefix, do_compress=False)
     return 1
- 
- 
-def multiprocess_logs(log_pairs, prefix):
+
+
+def multiprocess_logs(log_pairs, prefix, nick):
     pool = Pool(cpu_count())
     analyses = None
     try:
-        func = partial(analyze_func, prefix)
+        func = partial(analyze_func, prefix, nick)
         mr = pool.map_async(func, log_pairs)
         pool.close()
-        while not mr.ready(): mr.wait(1)
+        while not mr.ready():
+            mr.wait(1)
     except KeyboardInterrupt:
         logging.info("interrupted, terminating process pool")
         pool.terminate()
@@ -66,5 +74,3 @@ def multiprocess_logs(log_pairs, prefix):
         sys.exit()
     except Exception as e:
         logging.error(e)
-
-   
