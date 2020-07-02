adece5d55b8eb7eab03167f5bdd91330ba195b36
Karsten Loesing <karsten.loesing@gmx.net>
2020-07-02 21:06:31 +0200

    Bump version to 0.5.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
     install_requires = f.readlines()
 
 setup(name='OnionPerf',
-      version='0.3',
+      version='0.5',
       description='A utility to monitor, measure, analyze, and visualize the performance of Tor and Onion Services',
       author='Rob Jansen',
       url='https://github.com/robgjansen/onionperf/',
