ee00c37832f26d6d0df583a28f16ec315946daba
George Kadianakis <desnacked@riseup.net>
2020-11-12 13:05:33 +0200

    Improve the plotting codebase.

--- analysis_state_timeout.py
+++ analysis_state_timeout.py
@@ -10,9 +10,7 @@ from scipy.stats import pareto
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 
-# Read data from a Tor state file
-fname = sys.argv[1]
-data = []
+numpy.seterr('raise')
 
 def parse_state_line(line):
     """
@@ -36,49 +34,63 @@ def parse_state_line(line):
 
     return ([value] * occurences)
 
-# Extract data from state file
-data = []
-for line in open(fname,'r'):
-    values = parse_state_line(line)
-    if values:
-        data.extend(values)
+def extract_data(state_fname):
+    # Extract data from state file
+    data = []
+    for line in open(state_fname,'r'):
+        values = parse_state_line(line)
+        if values:
+            data.extend(values)
 
-# Now replace abandoned circuits with maximum timeout
-for i, value in enumerate(data):
-    if math.isnan(value):
-        data[i] = max(data)
+    # For each abandoned circuits turn it into a circuit that has reached the
+    # maximum timeout
+    for i, value in enumerate(data):
+        if math.isnan(value):
+            data[i] = max(data)
 
-print(data)
+    return data
 
-fig, ax = plt.subplots()
+def plot_state_file(data):
 
-# Plot the histogram
-bins = list(range(0, max(data), 100)) # bins should be every 100ms
-bins.append(max(data)) # also include the last one
+    # Make the bins for the histogram
+    bins = list(range(0, max(data), 100)) # bins should be every 100ms
+    bins.append(max(data)) # also include the last one
 
-#print(bins)
-plt.hist(data, bins=bins, density=True, facecolor='green', alpha=1)
+    # Plot the histogram
+    fig, ax = plt.subplots()
+    plt.hist(data, bins=bins, density=True, facecolor='green', alpha=1)
 
-numpy.seterr('raise')
+    # Try to fit a Pareto distribution to the data
+    shape, loc, scale = pareto.fit(data, 1, loc=0, scale=1)
+    y = pareto.pdf(bins, shape, loc=loc, scale=scale)
+    # Plot the pareto
+    l = plt.plot(bins, y, 'r--', linewidth=2)
 
-# Try to fit a Pareto in the data
-# shape is b (alpha in wikipedia), scale is x (x_b in wikipedia)
-shape, loc, scale = pareto.fit(data)
-y = pareto.pdf(bins, shape, loc=loc, scale=scale)
+    # Set up the graph metadata
+    plt.xticks((0,500, 1000, 1500) + tuple(range(2500, max(data), 5000)), rotation=90)
+    ax.grid(alpha=0.3)
+    plt.xlabel('Miliseconds')
+    plt.ylabel('Probability')
+    plt.title("Histogram of %d circuit timeout values fitted against Pareto with shape=%.3f, loc=%.3f and scale=%.3f" %(len(data), shape, loc, scale))
+    plt.grid(True)
 
-# Plot the Pareto on top of the bins
-l = plt.plot(bins, y, 'r--', linewidth=2)
+    # Plot it! :)
+    plt.show()
+    #basename=os.path.splitext(sys.argv[1])[0]
+    #plt.savefig(basename + "_pareto.png", dpi=300)
 
-plt.xticks((0,500, 1000, 1500) + tuple(range(2500, max(data), 5000)), rotation=90)
-ax.grid(alpha=0.3)
+def plot_all(state_fnames_list):
+    for state_fname in state_fnames_list:
+        data = extract_data(state_fname)
+        plot_state_file(data)
 
-#plot
-plt.xlabel('Miliseconds')
-plt.ylabel('Probability')
-plt.title("Histogram of %d circuit timeout values fitted against Pareto with shape=%.3f, loc=%.3f and scale=%.3f" %(len(data), shape, loc, scale))
-plt.grid(True)
+def main():
+    if len(sys.argv) < 2:
+        print("Usage:\n $ ./analysis_state_timeout.py state-1.txt state-2.txt state-3.txt")
+        sys.exit(1)
 
-#plt.show()
-basename=os.path.splitext(sys.argv[1])[0]
-plt.savefig(basename + "_pareto.png", dpi=300)
+    state_fnames_list = sys.argv[1:]
+    plot_all(state_fnames_list)
 
+if __name__ == '__main__':
+    main()
